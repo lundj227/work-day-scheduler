4b8f293f64e0a2dbcce8f6177cffb69813ad49a9
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-08-23 18:30:27 -0700

    Added framework for what happens when a different button is pushed

--- Assets/script.js
+++ Assets/script.js
@@ -11,6 +11,36 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var allInputs = document.querySelectorAll('.description');
+  const buttons = document.querySelectorAll('.saveBtn');
+  buttons.forEach(button => {
+    button.addEventListener('click', function(event){
+      event.preventDefault();
+      if(this.parentElement.id == 'hour-9'){
+        
+      }else if(this.parentElement.id == 'hour-10'){
+        
+      }else if(this.parentElement.id == 'hour-11'){
+        
+      }else if(this.parentElement.id == 'hour-12'){
+        
+      }else if(this.parentElement.id == 'hour-13'){
+        
+      }else if(this.parentElement.id == 'hour-14'){
+        
+      }else if(this.parentElement.id == 'hour-15'){
+        
+      }else if(this.parentElement.id == 'hour-16'){
+        
+      }else if(this.parentElement.id == 'hour-17'){
+        
+      }
+  })
+  
+    var savedText = document.querySelector('.description').value;
+    var setDescription = localStorage.setItem('hour-9', savedText);
+    document.querySelector('.description').textContent = setDescription;
+  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -22,11 +52,10 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  currentDay.textContent = dayjs().format('MMM D, YYYY hh:mm:ss a');
+
+  setInterval(function(){
+    var nowTime = dayjs().format('MMM D, YYYY hh:mm:ss a');
+    currentDay.textContent = nowTime;
+  }, 1000);
 });
-
-currentDay.textContent = dayjs().format('MMM D, YYYY hh:mm:ss a');
-
-setInterval(function(){
-  var nowTime = dayjs().format('MMM D, YYYY hh:mm:ss a');
-  currentDay.textContent = nowTime;
-}, 1000);
